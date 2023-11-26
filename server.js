1e0a00038f5c37911539201f954dbd4333c95ef3
sahana728 <138840925+sahana728@users.noreply.github.com>
2023-11-27 00:10:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const path = require('path');
+app.use(express.static(path.join(__dirname + '/Public')));
+
+io.on('connection', function (socket) {
+  socket.on('newuser', function (username) {
+    socket.broadcast.emit('update', username + ' joined the conversation');
+  });
+
+  socket.on('exituser', function (username) {
+    socket.broadcast.emit('update', username + ' left the conversation');
+  });
+
+  socket.on('chat', function (message) {
+    const messageWithEmoji = processEmoji(message);
+    socket.broadcast.emit('chat', messageWithEmoji);
+  });
+});
+
+
+  
+
+  return msg;
+
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
